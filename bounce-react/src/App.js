86303e54ac7c63d01f059865acbcf97bc33b5f59
Diogo <diogofcduarte@gmail.com>
2024-04-15 18:46:17 +0100

    Boostrap rails application with react front end

---
+++ bounce-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BookingPage from './components/BookingPage/BookingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <BookingPage></BookingPage>
+    </div>
+  );
+}
+
+export default App;
